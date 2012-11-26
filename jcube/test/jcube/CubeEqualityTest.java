ed04b82b3d2767fec75b9f88590c8979657d9f03
Benji <Benji@10.7.252.232>
2012-11-26 14:49:54 +0100

    New commit

--- jcube/test/jcube/CubeEqualityTest.java
+++ jcube/test/jcube/CubeEqualityTest.java
@@ -6,24 +6,11 @@ import org.junit.Test;
 
 public class CubeEqualityTest {
 
-<<<<<<< HEAD
 
 	@Test
 	public void twoCubeWithOneDifferentFaceShouldNotBeEqual() {
 		Cube cubeGit = (new Cube()).addFace("Git");
 		Cube cubeSVN = (new Cube()).addFace("SVN");
-		assertFalse(cubeGit.equals(cubeSVN));
-	}
-	@Test
-	public void twoCubeWithTwoDifferentFaceShouldNotBeEqual() {
-		Cube cubeGit = (new Cube()).addFace("Git").addFace("Face");
-		Cube cubeSVN = (new Cube()).addFace("SVN").addFace("Face2");
-=======
-	@Test
-	public void twoCubeWithOneDifferentFaceShouldNotBeEqual() {
-		Cube cubeGit = (new Cube()).addFace("Git");
-		Cube cubeSVN = (new Cube()).addFace("SVN");
->>>>>>> refs/remotes/origin/cours_iae
 		assertFalse(cubeGit.equals(cubeSVN));
 	}
 
--- jcube/test/jcube/CubeFromOneLineTextFile.java
+++ jcube/test/jcube/CubeFromOneLineTextFile.java
@@ -3,38 +3,6 @@ package jcube;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
-
-<<<<<<< HEAD
-
-import org.junit.Before;
-import org.junit.Test;
-
-
-public class CubeFromOneLineTextFile {
-	private Cube actualCube;
-	
-	@Before
-	public void loadCubeFromONeLineCubeDoTxt() throws Exception{
-		this.actualCube = Cube.fromTextFile("templates/one-line-cube.txt");
-	}
-	@Test
-	public void cubeShouldBeOneFaceWithCommentSortirDuCoursPlusTot() throws IOException {
-		Cube expectedCube = (new Cube())
-				.addFace("Comment sortir du cours plus tôt");
-		
-		assertEquals(expectedCube, actualCube);
-	}
-	@Test
-	public void FusionnerLeCubeEnXML() throws Exception{
-		String xml = actualCube.xmlFromSVGFile("templates/cube.svg");
-		XMLDocument readCubeXML = (new XMLDocument()).loadXMLString(xml);
-		assertTrue(readCubeXML.match("//tspan[contains(text(), \"Comment sortir du cours plus tôt\")]"));
-	}
-	
-
-}
-
-=======
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 
@@ -64,4 +32,3 @@ public class CubeFromOneLineTextFile {
 		assertTrue(docSVG.match("//tspan[contains(text(), 'Comment sortir du cours plus tot')]"));
 	}
 }
->>>>>>> refs/remotes/origin/cours_iae
